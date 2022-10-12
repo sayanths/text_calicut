50885cb93f0556b9513c984ae878349ea1a0b3da
sayanth A <sayanthsayu619@gmail.com>
2022-10-12 15:44:17 +0530

    splash_screen almost completed

--- lib/app/splash_screen/view/splash_screen.dart
+++ lib/app/splash_screen/view/splash_screen.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:login_test_calicut/app/splash_screen/view_model/splashscreen_controller.dart';
+import 'package:provider/provider.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-            child: Image.asset(
-      "assets/splash.jpg",
-      fit: BoxFit.cover,
-    )));
+    return Consumer<SplashScreenController>(
+      builder: (context, value, _) => Scaffold(
+          body: Center(
+              child: Image.asset(
+        "assets/splash.jpg",
+        fit: BoxFit.cover,
+      ))),
+    );
   }
 }
--- lib/app/splash_screen/view_model/splashscreen_controller.dart
+++ lib/app/splash_screen/view_model/splashscreen_controller.dart
@@ -1,5 +1,21 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:async';
 
-class SplashScreenController extends ChangeNotifier{
-  
+import 'package:flutter/material.dart';
+import 'package:login_test_calicut/app/login_page/view/login_page.dart';
+import 'package:login_test_calicut/app/routes/routes.dart';
+
+class SplashScreenController extends ChangeNotifier {
+  SplashScreenController() {
+    timing();
+  }
+
+  timing() {
+    Timer(
+      const Duration(seconds: 2),
+      () {
+        Routes.push(screen: const LoginPage());
+      },
+    );
+    notifyListeners();
+  }
 }
